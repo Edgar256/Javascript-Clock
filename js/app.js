323a9e056a353980ba4b7e8212b8357d5331ad72
Edgar256 <tinka.edgar@gmail.com>
2018-12-07 05:53:18 +0300

    added converting to 12 hr clock

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@
 
 $(document).ready(function(){
 
+
 	setInterval(function(){
 		var date = new Date();
 		var hrTime = date.getHours();
@@ -27,33 +28,43 @@ $(document).ready(function(){
 		document.getElementById("minClock").innerHTML = minTime;
 		document.getElementById("secClock").innerHTML = secTime;
 
+		
 	}, 1000);
 
-	/*function conv12(){
+	function conv12(){
 
-		setInterval(function(){
-			var date = new Date();
-			var hrTime = date.getHours();
-			var minTime = date.getMinutes();
-			var secTime= date.getSeconds();
+			setInterval(function(){
+				var date = new Date();
+				var hrTime = date.getHours();
+				var minTime = date.getMinutes();
+				var secTime= date.getSeconds();
+				var pm = 'pm';
+				var am = 'am';
 
-			var minTime0 = minTime.toString();
-			var secTime0 = secTime.toString();
+				var minTime0 = minTime.toString();
+				var secTime0 = secTime.toString();
 
-			if (minTime0.length < 2){
-				secTime = '0' + minTime;
-			};
+				if (minTime0.length < 2){
+					minTime = '0' + minTime;
+				};
 
-			if (secTime0.length < 2){
-				secTime = '0' + secTime;
-			};
+				if (secTime0.length < 2){
+					secTime = '0' + secTime;
+				};
 
-			document.getElementById("hrClock").innerHTML = hrTime;
-			document.getElementById("minClock").innerHTML = minTime;
-			document.getElementById("secClock").innerHTML = secTime;
+				document.getElementById("hrClock").innerHTML = hrTime;
+				document.getElementById("minClock").innerHTML = minTime;
+				document.getElementById("secClock").innerHTML = secTime;
 
-		}),1000);
+				if(hrTime > 12){
+					document.getElementById("amPm").innerHTML = pm;
+				}else{
+					document.getElementById("amPm").innerHTML = am;
+				}
 
-	};*/
-	
+			}, 1000);
+
+		};
+
 });
+
